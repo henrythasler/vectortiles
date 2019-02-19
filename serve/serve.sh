dc1dabbf611efc13d81f70473f3fbf58672aa315
henrythasler <git@thasler.org>
2019-02-19 21:23:50 +0100

    fixed server issues

--- serve/serve.sh
+++ serve/serve.sh
@@ -1,46 +1,96 @@
 #!/bin/bash
 
-### Configure names, folders, etc.
-dbpath="/media/mapdata/pgdata_mvt"
+# A POSIX variable
+OPTIND=1         # Reset in case getopts has been used previously in the shell.
+
+# Initialize our own variables:
+SERVER=false
+
+while getopts "h?s" opt; do
+    case "$opt" in
+    h|\?)
+        printf " -s for server version"
+        exit 0
+        ;;
+    s)  SERVER=true
+        ;;
+    esac
+done
+
+shift $((OPTIND-1))
+
+[ "${1:-}" = "--" ] && shift
+
+# globals
+pgdocker="postgis11"
 config="$(pwd)/config/cyclemap.toml"
 cache="$(pwd)/.cache"
 
-### Start postgis-container 
-if [ ! "$(docker ps -q -f name=${pgdocker})" ]; then
-    if [ "$(docker ps -aq -f status=exited -f name=${pgdocker})" ]; then
-        echo "removing old ${pgdocker} container"
-        docker rm ${pgdocker}
-    fi
-    # run container as current user
-    echo "starting ${pgdocker} container"
-    docker run -d \
-        --name ${pgdocker} \
-        --network gis \
-        -p 5432:5432 \
-        --user "$(id -u):$(id -g)" \
-        -v ${dbpath}:/pgdata \
-        -v $(pwd)/postgis.conf:/etc/postgresql/postgresql.conf \
-        -e PGDATA=/pgdata \
-        img-postgis:0.9 -c 'config_file=/etc/postgresql/postgresql.conf'
+mkdir -p ${cache}
 
-    ### Wait until startup is complete
-    # FIXME: fin some other solution to wait for completion
-    sleep 3s
-else echo "${pgdocker} container already running"
+if ${SERVER}
+then
+    printf "using server configuration\n"
+
+    dbpath="vectortiles_db" # volume container
+
+    if [ "$(sudo docker ps -q -f name=${pgdocker})" ]; then
+        ### start tileserver
+        sudo docker run \
+            -d \
+            --name tegola \
+            --network gis \
+            -p 8081:8080 \
+            --user "$(id -u):$(id -g)" \
+            -v ${config}:/data/config.toml:ro \
+            -v ${cache}:/cache \
+            tegola:master serve \
+                --config /data/config.toml
+    else echo "${pgdocker} container not running"
+    fi
+else
+    # localhost
+    ### Configure names, folders, etc.
+    dbpath="/media/mapdata/pgdata_mvt"
+
+    rm -rf ${cache}/local 
+
+    ### Start postgis-container 
+    if [ ! "$(docker ps -q -f name=${pgdocker})" ]; then
+        if [ "$(docker ps -aq -f status=exited -f name=${pgdocker})" ]; then
+            echo "removing old ${pgdocker} container"
+            docker rm ${pgdocker}
+        fi
+        # run container as current user
+        echo "starting ${pgdocker} container"
+        docker run -d \
+            --name ${pgdocker} \
+            --network gis \
+            -p 5432:5432 \
+            --user "$(id -u):$(id -g)" \
+            -v ${dbpath}:/pgdata \
+            -v $(pwd)/postgis.conf:/etc/postgresql/postgresql.conf \
+            -e PGDATA=/pgdata \
+            img-postgis:0.9 -c 'config_file=/etc/postgresql/postgresql.conf'
+
+        ### Wait until startup is complete
+        # FIXME: fin some other solution to wait for completion
+        sleep 3s
+    else echo "${pgdocker} container already running"
+    fi
+
+    ### start tileserver
+    docker run \
+        --rm \
+        --name tegola \
+        --network gis \
+        -p 8080:8080 \
+        --user "$(id -u):$(id -g)" \
+        -v ${config}:/data/config.toml:ro \
+        -v ${cache}:/cache \
+        -e TEGOLA_SQL_DEBUG=LAYER_SQL \
+        tegola:henry serve \
+            --config /data/config.toml
 fi
 
-mkdir -p ${cache}
-rm -rf ${cache}/global
-rm -rf ${cache}/local
-
-### start tileserver
-docker run \
-    --rm \
-    --network gis \
-    -p 8080:8080 \
-    --user "$(id -u):$(id -g)" \
-    -v ${config}:/data/config.toml:ro \
-    -v ${cache}:/cache \
-    -e TEGOLA_SQL_DEBUG=LAYER_SQL \
-    tegola:master serve \
-        --config /data/config.toml \
+#        -e TEGOLA_SQL_DEBUG=LAYER_SQL:EXECUTE_SQL \
