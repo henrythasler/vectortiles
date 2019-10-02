b9452b9393859cba6719b5ff2507b0ce524c144e
henrythasler <git@thasler.org>
2019-10-02 21:32:54 +0200

    move terraform-stuff to new repo https://github.com/henrythasler/cloud-osmdata

--- publish/terraform/scripts/download.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "starting up..."
-wget -qO- ${DOWNLOAD_URL} | aws s3 cp - s3://${GIS_DATA_BUCKET}/data/pbf/${OBJECT_NAME}
-echo "all done"
--- publish/terraform/scripts/production.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "starting up..."
-imposm import -mapping mapping.yaml -connection 'postgis://'${POSTGIS_USER}':'${PGPASSWORD}'@'${POSTGIS_HOSTNAME}'/'${DATABASE_NAME}'?prefix=NONE' -deployproduction
-echo "all done"
